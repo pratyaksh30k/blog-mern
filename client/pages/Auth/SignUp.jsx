426cd38d1846c0a70d44ad24a6fd4d72533079ab
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2024-07-17 18:46:45 +0530

    Add functionality to the sign up page

--- client/pages/Auth/SignUp.jsx
+++ client/pages/Auth/SignUp.jsx
@@ -1,18 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    setLoading(true);
+    setErrorMessage(null);
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/signin");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+      console.log(error);
+    }
+  };
   return (
     <div style={{ marginTop: "40px" }} className="min-h-screen">
       <div
         style={{
-          padding: "50px",
+          padding: "40px",
           gap: "40px",
           marginTop: "180px",
           marginBottom: "180px",
         }}
-        className="flex max-w-5xl mx-auto flex-col md:flex-row md:items-center gap border-gray-300 border border-solid rounded-lg shadow-lg"
+        className="flex max-w-3xl mx-auto flex-col md:flex-row md:items-center gap border-gray-300 border border-solid rounded-lg shadow-lg"
       >
         <div className="flex-1">
           <Link
@@ -31,14 +67,18 @@ export default function SignUp() {
           </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value="Your username" className="text-base font-semibold" />
+              <Label
+                value="Your username"
+                className="text-base font-semibold"
+              />
               <TextInput
                 type="text"
                 placeholder="Username"
                 id="username"
                 className="font-semibold placeholder:font-semibold"
+                onChange={onChange}
               />
             </div>
             <div>
@@ -48,23 +88,36 @@ export default function SignUp() {
                 placeholder="name@company.com"
                 id="email"
                 className="font-semibold placeholder:font-semibold"
+                onChange={onChange}
               />
             </div>
             <div>
-              <Label value="Your password" className="text-base font-semibold" />
+              <Label
+                value="Your password"
+                className="text-base font-semibold"
+              />
               <TextInput
                 type="password"
                 placeholder="Password"
                 id="password"
                 className="font-semibold placeholder:font-semibold"
+                onChange={onChange}
               />
             </div>
-            <Link
-              to="/signin"
+            <button
+              type="submit"
               className="text-center font-semibold cursor-pointer px-4 py-2 bg-gradient-to-r from-[#EF233D] to-[#F48F2A] text-white rounded-lg transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:bg-gradient-to-l"
+              disabled={loading}
             >
-              Sign Up
-            </Link>
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span style={{paddingLeft: "10px"}}>Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
+            </button>
             {/* <Button
               gradientDuoTone="purpleToPink"
               type="submit"
@@ -84,17 +137,17 @@ export default function SignUp() {
           <div style={{ marginTop: "12px" }} className="flex gap-2 text-sm">
             <span>Already have an account?</span>
             <Link
-              to="/sign-in"
+              to="/signin"
               className="text-blue-700 hover:underline font-semibold"
             >
               Sign In
             </Link>
           </div>
-          {/* {errorMessage && (
-            <Alert className="mt-5" color="failure">
+          {errorMessage && (
+            <Alert style={{ marginTop: "12px" }} color="failure">
               {errorMessage}
             </Alert>
-          )} */}
+          )}
         </div>
       </div>
     </div>
