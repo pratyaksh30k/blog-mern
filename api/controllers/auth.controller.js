d2cf996f434936eea6fd8d29fd9c0bbf40d65afa
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2024-07-18 16:56:41 +0530

    Create signin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async(req,res,next) =>{ //this is async because api needs some time to process and respond
   const {username,email,password} = req.body;
@@ -24,4 +25,35 @@ export const signup = async(req,res,next) =>{ //this is async because api needs 
     next(error);
   }
   
+}
+
+export const signin = async(req,res,next) =>{ //this is async because api needs some time to process and respond
+  const {email,password} = req.body;
+
+  if(!email || !password || email === "" || password === ""){
+    next(errorHandler(400,"All fields are required!"));
+  }
+
+  try{
+    const validUser = await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404,"User not found!"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword){
+      return next(errorHandler(400,"Invalid password!"));
+    }
+
+    const token = jwt.sign(
+      {id:validUser._id},process.env.JWT_SECRET,
+    );
+
+    const {password:pass,...rest} = validUser._doc; //To hide password and hashed password to be shown in the json.
+
+    res.status(200).cookie('access_token',token,{
+      httpOnly:true}).json(rest);
+  }catch(error){
+    next(error);
+  }
 }
