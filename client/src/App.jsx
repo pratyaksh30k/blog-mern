eada74d01ba85bc738ed33a6c8cef3d0d7afda3f
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2024-07-19 17:58:46 +0530

    Make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "../pages/Auth/SignIn.jsx";
 import Dashboard from "../pages/Dashboard/Dashboard.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 function App() {
   return (
     <BrowserRouter>
@@ -16,7 +17,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
